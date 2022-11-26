060e679f4351307afd7f8e2e55f95e0ecb4d0678
Tigran Mkrtchyan <tigranmkrtchyan59@gmail.com>
2022-11-26 19:12:53 +0000

    Completed javascript for loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+for (var t = 100; t > 0; t--) {
+  console.log('Time till explosion: ' + t + '!');
+}
